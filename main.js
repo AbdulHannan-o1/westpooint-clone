42fdc2faae098b80719ca3a8ee8535ef435c8608
CodeCrafted-1 <shaikhhannan729@gmail.com>
2025-03-27 03:51:29 +0500

    header edit

--- main.js
+++ main.js
@@ -186,3 +186,10 @@ function showPlaceholder(element) {
         element.placeholder = element.getAttribute("data-placeholder"); // Restore if empty
     }
 }
+ // Initialize popovers
+ document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+});
